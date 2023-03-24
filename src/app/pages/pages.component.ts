c24f8db38dcdfbb580d22b74353e5f549f265bb4
Erick Arechiga <earechigae@gmail.com>
2023-03-23 23:19:27 -0600

    Fin de la sección 3 - Diseño

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagres',
+  templateUrl: './pages.component.html',
+  styles: [
+  ]
+})
+export class PagesComponent {
+
+}
